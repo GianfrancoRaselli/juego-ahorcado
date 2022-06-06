2ec8cdb218adcf81f332ec5e914a3404d48d2600
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-06-06 20:02:38 -0300

    errores

--- Models/Juego.js
+++ Models/Juego.js
@@ -1,6 +1,7 @@
 module.exports = class Juego {
   constructor() {
     this.puntos = 0;
+    this.erroresPermitidos = 6; // cabeza, cuerpo, brazos, piernas
     this.letrasSeleccionadas = [];
   }
 
