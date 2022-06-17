460e3b6a5bc375a4beec141f590afbbbcc47fa08
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-06-17 20:05:55 -0300

    back

--- test/testMVP.js
+++ test/testMVP.js
@@ -14,7 +14,7 @@ beforeEach(() => {
 
 describe("Configuraciones iniciales del juego", () => {
   it("validar correcta instanciacion (creacion) del juego", () => {
-    assert.notEqual(null, null);
+    assert.notEqual(null, juego);
   });
 
   it("validar guardado de palabra a adivinar", () => {
