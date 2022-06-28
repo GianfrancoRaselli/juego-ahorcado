a46803e2b5261c22a4b9c5c8611499997db0f1f7
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-06-28 19:47:18 -0300

    .

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 
 //routes
 app.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Server listening' });
+    return res.status(200).json({ message: 'Server listeningg' });
 });
 app.use('/juego', require('./routes/juego'));
 
