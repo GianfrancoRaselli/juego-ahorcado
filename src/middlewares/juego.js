b0c1f6e2f1f77b5d6145a98394f7a83e21edfe2b
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-07-20 16:59:32 -0300

    .

--- src/middlewares/juego.js
+++ src/middlewares/juego.js
@@ -3,6 +3,6 @@ const { juego } = require('../controllers/juego');
 const juegoIniciado = async (req, res, next) => {
     if (!juego) return res.status(200).json("Partida no iniciada");
     return next();
-}
+};
 
 module.exports = { juegoIniciado };
