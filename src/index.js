bbcd5486ea944a0980c677f07d151a9010615f54
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-07-21 19:48:58 -0300

    last try

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Server listening" });
 });
-app.use('/juego', require("./routes/juego"));
+app.use("/juego", require("./routes/juego"));
 
 // starting the server
 app.listen(process.env.PORT, () => {
